044e49fb3ac1b7ed36e41c94c70556ed2db70d9e
ianrios <ian.rios1a@gmail.com>
2019-12-13 10:23:02 -0500

    added fix

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,8 +6,7 @@ function App() {
 	return (
 		<div className="app">
 			<h1>This is the Sustainability Scorecard Website.</h1>
-			<p>Click to view our <Modal buttonLabel="Point System"></Modal> </p>
-				 
+			<p>Click to view our <Modal buttonLabel="Point System"></Modal></p>
 		</div>
 	);
 }
